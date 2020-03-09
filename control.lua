9d153ff51016b1b257f8c8412b43d9837f15e52e
ruspartisan <bakulinm@gmail.com>
2020-03-09 23:08:23 +0300

    Added delivery history. Only deliveries of items that were not used in the last N deliveries can be shown

--- control.lua
+++ control.lua
@@ -2,6 +2,15 @@ disabled = "disbled"
 looking_for_train = "looking_for_train"
 following_train = "following_train"
 
+function has_value (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+
 function FollowOnTick(event)
 	local ticks_elapsed = event.tick - global.train_follow_start_tick
 	local locomotive_position = global.followed_train.locomotives.front_movers[1].position
@@ -25,12 +34,23 @@ end
 
 function OnDispatcherUpdated(event)
 	if global.screensaver_state == looking_for_train and table_size(event.train.schedule.records) ~= 1 then
+		local item = nil
+		for index, wait_condition in pairs(event.train.schedule.records[2].wait_conditions) do
+			if wait_condition.condition ~= nil then
+				item = 	wait_condition.condition.first_signal.name
+				break
+			end
+		end
+		if has_value(global.delivery_history, item) == true then
+			return
+		end
+		global.delivery_history[global.delivery_history_pointer] = item
+		global.delivery_history_pointer = (global.delivery_history_pointer + 1) % global.delivery_history_size 
 		script.on_event({defines.events.on_train_schedule_changed}, nil)
 		global.screensaver_state = following_train
 		global.followed_train = event.train
 		global.train_left_the_depot = false
 		global.train_follow_start_tick = game.tick
-		--game.print(table.tostring(global.followed_train.schedule))
 		script.on_event({defines.events.on_tick}, FollowOnTick)
 		if global.character == nil then
 			global.character = game.get_player(1).character
@@ -43,9 +63,12 @@ end
 function toggle_screensaver(event)
 	if global.screensaver_state == disabled then
 		game.print("Turning on screensaver. Press CTRL+S to disable.")
-		global.transition_time = 600
-		script.on_event({defines.events.on_train_schedule_changed}, OnDispatcherUpdated)
+		global.delivery_history = {}
+		global.delivery_history_size = 10
+		global.delivery_history_pointer = 0
+		global.transition_time = 300
 		global.screensaver_state = looking_for_train
+		script.on_event({defines.events.on_train_schedule_changed}, OnDispatcherUpdated)
 	else
 		game.print("Turning off screensaver.")
 		script.on_event({defines.events.on_train_schedule_changed}, nil)
